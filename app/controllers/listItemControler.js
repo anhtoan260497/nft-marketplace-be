e0d4e02dd0b832636a4d31a84346ceac3249c90d
anhtoan260497 <anhtoan260497@gmail.com>
2024-04-05 21:01:46 +0700

    add buy routes and listener

--- app/controllers/listItemControler.js
+++ app/controllers/listItemControler.js
@@ -3,6 +3,7 @@ const ListedItemModel = require("../models/ListedItemModel");
 
 class listItemControler {
     async postItem(req, res) {
+        console.log('list item', req.body)
         try {
             if (!req.body.confirmed || req.body.logs.length === 0) {
                 res.send('done')
@@ -12,10 +13,10 @@ class listItemControler {
             const listItem = new ListedItemModel({
                 ...parseData[0],
                 tokenId: formatEtherCustom(parseData[0].tokenId),
-                price: formatEtherCustom(parseData[0].price, 18),
+                price: parseData[0].price,
+                priceFormat: formatEtherCustom(parseData[0].price, 18),
                 chainId: req.body.chainId
             })
-            console.log('listitem', listItem)
             await listItem.save()
             res.send('done')
         } catch (err) {
@@ -25,12 +26,37 @@ class listItemControler {
 
     async getItem(req, res) {
         try {
-            const listItems = await ListedItemModel.find({ chainId: req.params.chainId })
+            const listItems = await ListedItemModel.find({ chainId: req.params.chainId, deleted: false })
             res.json(listItems)
         } catch (err) {
             console.log(err)
             res.status(404).json({ message: 'error' })
         }
+    }
+
+    async buyItem(req, res) {
+        let restoreFlag = null
+        if (!req.body.confirmed) {
+            const parseData = decodeLogs(req.body)
+            const item = await ListedItemModel.findOne({ tokenId: parseData[0].tokenId, chainId: req.body.chainId, nftAddress: parseData[0].NftAddress })
+            console.log('not confirmed', item)
+            await item.delete()
+
+            if (restoreFlag) return;
+            restoreFlag = setTimeout(async () => {
+                const item = await ListedItemModel.findOne({ tokenId: parseData[0].tokenId, chainId: req.body.chainId, nftAddress: parseData[0].NftAddress })
+                if(item) await item.restore()
+            }, 1800000);
+
+            return
+        }
+
+        if (req.body.confirmed) {
+            if(restoreFlag) clearTimeout(restoreFlag)
+            const parseData = decodeLogs(req.body)
+            await ListedItemModel.deleteOne({ tokenId: parseData[0].tokenId, chainId: req.body.chainId, nftAddress: parseData[0].NftAddress })
+        }
+        res.send('Done')
     }
 }
 
