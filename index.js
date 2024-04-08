006f05fd1c135f0a03000aff1d9914d2ce7f4cd9
anhtoan260497 <anhtoan260497@gmail.com>
2024-04-09 00:41:22 +0700

    add deploy

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ const db = require('./app/config/db')
 const runMoralis = require('./moralis/moralis')
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+const ngrok = require('@ngrok/ngrok')
+require('dotenv').config()
 
 routes(app)
 db.connect()
 // runMoralis()
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`)
+  
+  try {
+    const url = await ngrok.connect({
+      addr: 8080,
+      domain: process.env.DOMAIN,
+      authtoken: process.env.AUTH_NGROK
+    })
+    console.log('Ngrok', url)
+  } catch (err) {
+    console.log('err', err)
+  }
 })
